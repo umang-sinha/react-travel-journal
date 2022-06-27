444309f7a5bbdb72d6fd647c18729f47853bafe5
Umang Sinha <umangsinha12@gmail.com>
2022-06-27 20:20:08 +0530

    fix: add components to app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Navbar from "./components/Navbar"
+import Body from "./components/Body"
+
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      <Body />
     </div>
   );
 }
